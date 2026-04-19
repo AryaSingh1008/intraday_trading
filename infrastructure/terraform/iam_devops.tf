f352038f7456ed769d72fdbd57d1f328719e2167
AryaSingh1008 <singh.arya1097@gmail.com>
2026-04-19 11:44:00 +0530

    fix: add s3:PutObjectTagging to codebuild-role for frontend object tagging

--- infrastructure/terraform/iam_devops.tf
+++ infrastructure/terraform/iam_devops.tf
@@ -95,7 +95,7 @@ data "aws_iam_policy_document" "codebuild_permissions" {
     effect = "Allow"
     actions = [
       "s3:GetObject", "s3:GetObjectVersion", "s3:PutObject",
-      "s3:ListBucket", "s3:DeleteObject",
+      "s3:ListBucket", "s3:DeleteObject", "s3:PutObjectTagging",
     ]
     resources = [
       aws_s3_bucket.pipeline_artifacts.arn,
